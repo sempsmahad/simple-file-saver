c9d5a61ae2efbe06503ca1ef4891cec63c007b5b
muhammad <mhdsemps@gmail.com>
2023-02-28 16:35:14 +0300

    upload button added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
 
+const FileUploadButton = () => {
+  const [file, setFile] = useState(null);
+
+  const handleFileSelect = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append('file', file);
+    axios
+      .post('/upload', formData)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return (
+    <div>
+      <input type='file' onChange={handleFileSelect} />
+      <button onClick={handleFormSubmit}>Upload</button>
+    </div>
+  );
+};
+
+ReactDOM.render(<FileUploadButton />, document.getElementById('root'));
